d4a1a28570e8d37347b5990e28849094f5f7d707
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-21 17:35:16 +0200

    asking for electricity percentage

--- Assignment1.py
+++ Assignment1.py
@@ -2,4 +2,5 @@ salary=input(int("Please enter your salary for the month: "))
 month=input("Please enter the name of the month: ")
 savingsPercentage=input(float("Please enter the percentage for your savings: "))
 rentPercentage=input(float("Please enter the percentage for your rent: "))
+electricityPercentage=input(float("Please enter the percentage for your electricity: "))
 
