11cbfc92d4f1afafc82118e436265211e723ec5b
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-21 17:32:40 +0200

    asking for rent savings

--- Assignment1.py
+++ Assignment1.py
@@ -1,3 +1,5 @@
 salary=input(int("Please enter your salary for the month: "))
-Month=input("Please enter the name of the month: ")
+month=input("Please enter the name of the month: ")
 savingsPercentage=input(float("Please enter the percentage for your savings: "))
+rentPercentage=input(float("Please enter the percentage for your rent: "))
+
