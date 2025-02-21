3faa729166fa2e4ca40cc9c5216d8474b360ed13
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-21 17:30:10 +0200

    asking for savings percentage

--- Assignment1.py
+++ Assignment1.py
@@ -1,2 +1,3 @@
 salary=input(int("Please enter your salary for the month: "))
 Month=input("Please enter the name of the month: ")
+savingsPercentage=input(float("Please enter the percentage for your savings: "))
