1e99644c9ea7f6cad947d801c1de46d49d12aa21
Orwa Dakdouk <orwadakdouk@gmail.com>
2025-02-21 17:36:22 +0200

    making the salary a float instead of integer

--- Assignment1.py
+++ Assignment1.py
@@ -1,4 +1,4 @@
-salary=input(int("Please enter your salary for the month: "))
+salary=input(float("Please enter your salary for the month: "))
 month=input("Please enter the name of the month: ")
 savingsPercentage=input(float("Please enter the percentage for your savings: "))
 rentPercentage=input(float("Please enter the percentage for your rent: "))
